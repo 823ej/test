1952c286a3d8a3902e413b692c264f2ea6af1f5d
823ej <64511734+823ej@users.noreply.github.com>
2020-08-27 02:20:18 +0900

    Add files via upload

---
+++ 숫자야구.js
@@ -0,0 +1,77 @@
+var 바디 = document.body;
+var 제목 = document.createElement('h1');
+바디.append(제목);
+제목.textContent = '너무나 재미있는 숫자 야구 게임';
+
+var 숫자배열 = [];
+var 숫자뽑기 = function(){
+    var 숫자후보 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for(var i = 0; i < 4; i +=1){
+        var 뽑은것 = 숫자후보.splice(Math.floor(Math.random() * (9-i)), 1)[0];
+        숫자배열.push(뽑은것);
+    }
+}
+
+숫자뽑기();
+console.log(숫자배열);
+var 결과 = document.createElement('h1');
+    바디.append(결과);
+var 폼 = document.createElement('form');
+document.body.append(폼);
+var 입력창 = document.createElement('input');
+입력창.type = 'number';
+폼.append(입력창);
+입력창.type = 'text';
+입력창.maxLength = 4;
+var 버튼 = document.createElement('button');
+버튼.textContent = '입력!';
+폼.append(버튼);
+var 틀린횟수 = 0;
+var 안내 = document.createElement('h2');
+    바디.append(안내);
+  
+  document.write('컴퓨터가 1에서 9까지의 숫자 중 임의로 4개를 골랐습니다. 맞혀보시겠어요? 숫자의 위치가 정확하게 맞으면 스트라이크, 위치는 달라도 4개 안에 있으면 볼입니다. 숫자와 그 위치를 정확히 맞혀 홈런을 노려 보세요!');
+
+
+폼.addEventListener('submit', function 비동기(이벤트){
+이벤트.preventDefault();
+
+var 답 = 입력창.value;
+
+
+
+if (답 === 숫자배열.join('')){
+결과.textContent = '홈런';
+입력창.value = '';
+입력창.focus();
+숫자뽑기();
+
+} else{ //답이 틀리면
+   
+var 답배열 = 답.split('');
+var 스트라이크 = 0;
+var 볼 = 0;
+틀린횟수 +=1;
+
+if(틀린횟수 >10){ //10번 틀린 경우
+    결과.textContent = '10번 넘게 틀려서 실패! 답은'+숫자배열.join(',') + '였습니다!';
+    안내.textContent = 'F5를 눌러 다시 해 보세요.';
+    입력창.value = '';
+    입력창.focus();
+    
+}else{//10번 미만 틀린 경우
+    안내.textContent = '현재 ' + 틀린횟수 + '번 틀렸습니다.';
+    console.log('답이 틀리면');
+for(var i = 0; i <4; i+=1){
+    if (Number(답배열[i]) === 숫자배열[i]){
+        스트라이크 +=1;
+    }else if(숫자배열.indexOf(Number(답배열[i])) > -1){
+        볼 += 1;}
+    }
+    
+    
+    결과.textContent = 스트라이크 + '스트라이크 ' + 볼 + '볼입니다.';
+    입력창.value = '';
+입력창.focus();
+}}
+});
