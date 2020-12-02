e62a47f53ce6d4691bfb96cecf3e809d64490709
823ej <64511734+823ej@users.noreply.github.com>
2020-12-02 15:04:24 +0900

    Add files via upload

--- p5js/종다리/sketch.js
+++ p5js/종다리/sketch.js
@@ -1,18 +1,31 @@
+// let tts1, tts2;
+
 function preload(){
-  img1 = loadImage('https://github.com/823ej/test/blob/master/p5js/%EC%A2%85%EB%8B%A4%EB%A6%AC/text.png');
-  img2 = loadImage('https://github.com/823ej/test/blob/master/p5js/%EC%A2%85%EB%8B%A4%EB%A6%AC/text2.png');
-  //  tts1 = loadSound('tts1.mp3');
-  //  tts2 = loadSound('tts2.mp3');
+  img1 = loadImage('text.png');
+  img2 = loadImage('text2.png');
+  // tts1 = loadSound('tts1.mp3');
+  // tts2 = loadSound('tts2.mp3');
 }
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  
+  var canvas1 = createCanvas(windowWidth, windowHeight, WEBGL);
+  // canvas1.mousePressed(playSound);
 }
+// function playSound() {
+//   if (mouseX < (windowWidth/2)) {
+//     tts1.play();
+//   } else {
+//     tts2.play();
+ 
+//   }
+  
+// }
 
 function draw() {
   background(255);
   orbitControl();
-   // rotateY(frameCount * 0.01);
-  //rotateY(-((mouseX)*0.00205));
+ // rotateY(frameCount * 0.01);
+ // rotateY(-((winmouseX)*0.00205));
 
     push();
  rotateY(61.26105);
@@ -26,5 +39,6 @@ translate(0,0,-0.01);
   texture(img2);
   plane((windowWidth), (windowHeight));
   pop();
-
 }
+
+
