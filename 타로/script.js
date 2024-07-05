6dcde858730dbffac20b18972d090894a8ab7dce
823ej <64511734+823ej@users.noreply.github.com>
2024-07-06 02:38:01 +0900

    Update script.js

--- 타로/script.js
+++ 타로/script.js
@@ -10,7 +10,6 @@ function pickup_click() {
     if (shownResult) return; // 이미 결과를 보여준 경우, 더 이상 동작하지 않음
     shownResult = true;
 
-    // 세 장의 카드를 랜덤하게 뽑아서 이미지와 결과 텍스트를 설정
     let cardNumbers = [1, 2, 3];
     cardNumbers.forEach(async cardNumber => {
         let cardImageElement = document.getElementById(`pick${cardNumber}`);
@@ -25,8 +24,9 @@ function pickup_click() {
 
         if (cardData.reversed && cardData.reversed.hasOwnProperty(cardIndex.toString())) {
             resultTextElement.textContent = cardData.reversed[cardIndex];
-            // 역방향 카드 이미지로 설정
-            cardImageElement.src = `img/reverse/card${cardIndex}.png`;
+            // 역방향 카드 이미지로 설정하고, 이미지를 뒤집기 위한 CSS 클래스 추가
+            cardImageElement.src = `img/card${cardIndex}.png`;
+            cardImageElement.classList.add('reversed'); // CSS에서 정의한 클래스 이름
         } else {
             resultTextElement.textContent = cardData[cardIndex];
             // 정방향 카드 이미지로 설정
