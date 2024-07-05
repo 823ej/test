ae3e3ff655265c4c2e97d33ab530149d1ba0d255
823ej <64511734+823ej@users.noreply.github.com>
2024-07-06 03:06:02 +0900

    Update script.js

--- 타로/script.js
+++ 타로/script.js
@@ -40,23 +40,17 @@ function cardClick(cardIndex) {
     const cardIds = ["pick1", "pick2", "pick3"];
     const textIds = ["textSpace1", "textSpace2", "textSpace3"];
     const timePeriods = ["past", "present", "future"];
-
-    // 무작위로 정방향 또는 역방향 선택
-    const isReversed = Math.random() < 0.5;
+    
+    const ForR = Math.random() < 0.5;
 
     const imgElement = document.getElementById(cardIds[cardIndex]);
     const textElement = document.getElementById(textIds[cardIndex]);
-
+    
     imgElement.src = `img/${num[cardIndex]}.png`;
-
-    // 선택된 방향에 따라 데이터 설정
-    const direction = isReversed ? "reversed" : "upright";
+    const direction = ForR ? "upright" : "reversed";
     textElement.innerText = tarotData[num[cardIndex]][direction][timePeriods[cardIndex]];
-
-    // 역방향인 경우 이미지를 회전
-    if (isReversed) {
+    
+    if (!ForR) {
         imgElement.style.transform = "rotate(180deg)";
-    } else {
-        imgElement.style.transform = "none";
     }
 }
